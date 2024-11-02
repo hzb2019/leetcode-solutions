66b0074b3ed8f8be31277f46c604f708fb4bbc09
hzb2019 <hzb2024@protonmail.com>
2024-11-01 21:12:30 -0400

    added 11-1-2024 daily problem

---
+++ isCircularSentence.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/circular-sentence
+// Solution beats 100% in runtime (<1ms) and 96.30% in memory usage (8.10MB)
+
+class Solution {
+public:
+
+    /**
+    *  compares last letter of each word in sentence to first letter of
+    *  each following word in a circular manner
+    *
+    *  @param sentence  string without leading or trailing spaces of alpha
+    *                   characters with spaces to separate words
+    *  @return          false if the last letter of a word does not match the first 
+    *                   letter of the following word, true otherwise
+    */
+    bool isCircularSentence(string& sentence) {
+        char previousLetter = sentence[0];
+        bool wasSpace = false;
+
+        for(char ch : sentence) {
+            if(wasSpace) {
+                if(ch != previousLetter) return false;
+                wasSpace = false;
+            }
+            
+            if(ch != ' ') previousLetter = ch;
+            else wasSpace = true;
+        }
+
+	// completes the circular check
+        if(previousLetter != sentence[0]) return false;
+
+        return true;
+    }
+};
