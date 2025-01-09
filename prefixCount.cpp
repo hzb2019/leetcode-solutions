4d2e096b325336a2b114a90fb4d699d469a47256
hzb2019 <hzb2024@protonmail.com>
2025-01-09 16:21:19 -0500

    01-09-2025 daily: 2185. Counting Words With a Given Prefix

---
+++ prefixCount.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/move-pieces-to-obtain-a-string
+// Solution beats 100% in runtime (<1ms) and 75.23% in memory usage (13.10MB)
+
+class Solution {
+public:
+    int prefixCount(const vector<string>& words, const string& pref) {
+        int count = 0;
+
+        for(const string& word : words) {
+            if(word.front() == pref.front()) {
+                if(pref.compare(word.substr(0, pref.size())) == 0) count ++;
+            }
+        }
+
+        return count;
+    }
+};
